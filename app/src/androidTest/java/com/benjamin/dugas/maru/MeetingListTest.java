24f7fe9d722a155d9e2e2beb3d10bc54c9bcc611
Larisme <mbenjamindugas1@hotmail.fr>
2020-12-29 11:38:51 +0100

    commit

--- app/src/androidTest/java/com/benjamin/dugas/maru/MeetingListTest.java
+++ app/src/androidTest/java/com/benjamin/dugas/maru/MeetingListTest.java
@@ -1,6 +1,7 @@
 package com.benjamin.dugas.maru;
 
 import android.content.Context;
+import android.view.MenuItem;
 import android.widget.EditText;
 
 import androidx.test.espresso.contrib.RecyclerViewActions;
@@ -26,13 +27,23 @@ import org.junit.runner.RunWith;
 import java.util.ArrayList;
 import java.util.List;
 
+import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 import static com.benjamin.dugas.maru.utils.RecyclerViewItemCountAssertion.withItemCount;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.startsWith;
 import static org.hamcrest.core.IsNull.notNullValue;
 import static org.junit.Assert.*;
 
@@ -60,80 +71,68 @@ public class MeetingListTest {
 
     @Test
     public void clickOnActionBouton_shouldShowAddMeeting() {
-        onView(ViewMatchers.withId(R.id.fab_add)).perform(click());
-        onView(ViewMatchers.withId(R.id.add)).check(matches(ViewMatchers.isDisplayed()));
-    }
-
-    @Test
-    public void meetingShouldShowOnListMeeting() {
-        int count = service.getMeeting().size();
-        onView(ViewMatchers.withId(R.id.meeting_list)).check(withItemCount(count));
-        onView(ViewMatchers.withId(R.id.fab_add)).perform(click());
-        onView(ViewMatchers.withId(R.id.tiet_topic)).perform(typeText("Test"), closeSoftKeyboard());
-        onView(ViewMatchers.withId(R.id.rb_f)).perform(click());
-        onView(ViewMatchers.withId(R.id.rb_10)).perform(click());
-        onView(ViewMatchers.withId(R.id.tiet_participant)).perform(typeText("Test1"), closeSoftKeyboard());
-        onView(ViewMatchers.withId(R.id.b_add)).perform(click());
-        onView(ViewMatchers.withId(R.id.tiet_participant)).perform(typeText("Test2"), closeSoftKeyboard());
-        onView(ViewMatchers.withId(R.id.b_create)).perform(click());
-        onView(ViewMatchers.withId(R.id.meeting_list)).check(withItemCount(count+1));
+        onView(withId(R.id.fab_add)).perform(click());
+        onView(withId(R.id.add)).check(matches(ViewMatchers.isDisplayed()));
     }
 
     @Test
     public void myMeetingList_deleteAction_shouldRemoveItem() {
         int count = service.getMeeting().size();
-        onView(ViewMatchers.withId(R.id.meeting_list)).check(withItemCount(count));
-        onView(ViewMatchers.withId(R.id.fab_add)).perform(click());
-        onView(ViewMatchers.withId(R.id.tiet_topic)).perform(typeText("Test"), closeSoftKeyboard());
-        onView(ViewMatchers.withId(R.id.rb_f)).perform(click());
-        onView(ViewMatchers.withId(R.id.rb_10)).perform(click());
-        onView(ViewMatchers.withId(R.id.tiet_participant)).perform(typeText("Test1"), closeSoftKeyboard());
-        onView(ViewMatchers.withId(R.id.b_add)).perform(click());
-        onView(ViewMatchers.withId(R.id.tiet_participant)).perform(typeText("Test2"), closeSoftKeyboard());
-        onView(ViewMatchers.withId(R.id.b_create)).perform(click());
-        onView(ViewMatchers.withId(R.id.meeting_list)).check(withItemCount(count+1));
-        onView(ViewMatchers.withId(R.id.meeting_list)).perform(RecyclerViewActions.actionOnItemAtPosition(0, new DeleteViewAction()));
-        onView(ViewMatchers.withId(R.id.meeting_list)).check(withItemCount(count));
+        onView(withId(R.id.meeting_list)).check(withItemCount(count));
+        onView(withId(R.id.fab_add)).perform(click());
+        onView(withId(R.id.tiet_topic)).perform(typeText("Test"), closeSoftKeyboard());
+        onView(withId(R.id.rb_f)).perform(click());
+        onView(withId(R.id.rb_10)).perform(click());
+        onView(withId(R.id.tiet_participant)).perform(typeText("Test1"), closeSoftKeyboard());
+        onView(withId(R.id.b_add)).perform(click());
+        onView(withId(R.id.tiet_participant)).perform(typeText("Test2"), closeSoftKeyboard());
+        onView(withId(R.id.b_create)).perform(click());
+        onView(withId(R.id.meeting_list)).check(withItemCount(count+1));
+        onView(withId(R.id.meeting_list)).perform(RecyclerViewActions.actionOnItemAtPosition(0, new DeleteViewAction()));
+        onView(withId(R.id.meeting_list)).check(withItemCount(count));
     }
 
     @Test
     public void myMeetingList_filterAction_shouldShowFilterItem() {
         int count = service.getMeeting().size();
-        onView(ViewMatchers.withId(R.id.meeting_list)).check(withItemCount(count));
-        onView(ViewMatchers.withId(R.id.fab_add)).perform(click());
-        onView(ViewMatchers.withId(R.id.tiet_topic)).perform(typeText("Test"), closeSoftKeyboard());
-        onView(ViewMatchers.withId(R.id.rb_f)).perform(click());
-        onView(ViewMatchers.withId(R.id.rb_10)).perform(click());
-        onView(ViewMatchers.withId(R.id.tiet_participant)).perform(typeText("Test1"), closeSoftKeyboard());
-        onView(ViewMatchers.withId(R.id.b_add)).perform(click());
-        onView(ViewMatchers.withId(R.id.tiet_participant)).perform(typeText("Test2"), closeSoftKeyboard());
-        onView(ViewMatchers.withId(R.id.b_create)).perform(click());
-        onView(ViewMatchers.withId(R.id.meeting_list)).check(withItemCount(count+1));
-        onView(ViewMatchers.withId(R.id.fab_add)).perform(click());
-        onView(ViewMatchers.withId(R.id.tiet_topic)).perform(typeText("Test1"), closeSoftKeyboard());
-        onView(ViewMatchers.withId(R.id.rb_f)).perform(click());
-        onView(ViewMatchers.withId(R.id.rb_10)).perform(click());
-        onView(ViewMatchers.withId(R.id.tiet_participant)).perform(typeText("Test3"), closeSoftKeyboard());
-        onView(ViewMatchers.withId(R.id.b_add)).perform(click());
-        onView(ViewMatchers.withId(R.id.tiet_participant)).perform(typeText("Test4"), closeSoftKeyboard());
-        onView(ViewMatchers.withId(R.id.b_create)).perform(click());
-        onView(ViewMatchers.withId(R.id.meeting_list)).check(withItemCount(count+2));
-        onView(ViewMatchers.withId(R.id.fab_add)).perform(click());
-        onView(ViewMatchers.withId(R.id.tiet_topic)).perform(typeText("Test2"), closeSoftKeyboard());
-        onView(ViewMatchers.withId(R.id.rb_a)).perform(click());
-        onView(ViewMatchers.withId(R.id.rb_13)).perform(click());
-        onView(ViewMatchers.withId(R.id.tiet_participant)).perform(typeText("Test5"), closeSoftKeyboard());
-        onView(ViewMatchers.withId(R.id.b_add)).perform(click());
-        onView(ViewMatchers.withId(R.id.tiet_participant)).perform(typeText("Test6"), closeSoftKeyboard());
-        onView(ViewMatchers.withId(R.id.b_create)).perform(click());
-        onView(ViewMatchers.withId(R.id.meeting_list)).check(withItemCount(count+3));
-        onView(ViewMatchers.withId(R.id.action_settings)).perform(click());
+        onView(withId(R.id.meeting_list)).check(withItemCount(count));
+        onView(withId(R.id.fab_add)).perform(click());
+        onView(withId(R.id.tiet_topic)).perform(typeText("Test"), closeSoftKeyboard());
+        onView(withId(R.id.rb_f)).perform(click());
+        onView(withId(R.id.rb_10)).perform(click());
+        onView(withId(R.id.tiet_participant)).perform(typeText("Test1"), closeSoftKeyboard());
+        onView(withId(R.id.b_add)).perform(click());
+        onView(withId(R.id.tiet_participant)).perform(typeText("Test2"), closeSoftKeyboard());
+        onView(withId(R.id.b_create)).perform(click());
+        onView(withId(R.id.meeting_list)).check(withItemCount(count+1));
+        onView(withId(R.id.fab_add)).perform(click());
+        onView(withId(R.id.tiet_topic)).perform(typeText("Test1"), closeSoftKeyboard());
+        onView(withId(R.id.rb_f)).perform(click());
+        onView(withId(R.id.rb_10)).perform(click());
+        onView(withId(R.id.tiet_participant)).perform(typeText("Test3"), closeSoftKeyboard());
+        onView(withId(R.id.b_add)).perform(click());
+        onView(withId(R.id.tiet_participant)).perform(typeText("Test4"), closeSoftKeyboard());
+        onView(withId(R.id.b_create)).perform(click());
+        onView(withId(R.id.meeting_list)).check(withItemCount(count+2));
+        onView(withId(R.id.fab_add)).perform(click());
+        onView(withId(R.id.tiet_topic)).perform(typeText("Test2"), closeSoftKeyboard());
+        onView(withId(R.id.rb_a)).perform(click());
+        onView(withId(R.id.rb_13)).perform(click());
+        onView(withId(R.id.tiet_participant)).perform(typeText("Test5"), closeSoftKeyboard());
+        onView(withId(R.id.b_add)).perform(click());
+        onView(withId(R.id.tiet_participant)).perform(typeText("Test6"), closeSoftKeyboard());
+        onView(withId(R.id.b_create)).perform(click());
+        onView(withId(R.id.meeting_list)).check(withItemCount(count+3));
+        onView(withId(R.id.action_settings)).perform(click());
+//        onView(withId(R.id.menuItem_hour)).check(matches(withText(containsString("Hour"))));
+//        onData(allOf(instanceOf(String.class), startsWith("Hour"))).perform(click());
+        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
         onView(ViewMatchers.withId(R.id.menuItem_hour)).perform(click());
-        onView(ViewMatchers.withId(R.id.i_10h00)).perform(click());
-        onView(ViewMatchers.withId(R.id.meeting_list)).check(withItemCount(count+1));
-        onView(ViewMatchers.withId(R.id.action_settings)).perform(click());
-        onView(ViewMatchers.withId(R.id.menuItem_location)).perform(click());
-        onView(ViewMatchers.withId(R.id.i_a)).perform(click());
-        onView(ViewMatchers.withId(R.id.meeting_list)).check(withItemCount(count+2));
+        onView(withId(R.id.i_10h00)).perform(click());
+        onView(withId(R.id.meeting_list)).check(withItemCount(count+1));
+        onView(withId(R.id.action_settings)).perform(click());
+        onView(withId(R.id.menuItem_location)).perform(click());
+        onView(withId(R.id.i_a)).perform(click());
+        onView(withId(R.id.meeting_list)).check(withItemCount(count+2));
     }
 }
