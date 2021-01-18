9e5aa2ccb507b7455fb2273913a606ff3be3178f
Larisme <mbenjamindugas1@hotmail.fr>
2021-01-18 14:42:38 +0100

    Final vers.

--- app/src/androidTest/java/com/benjamin/dugas/maru/MeetingListTest.java
+++ app/src/androidTest/java/com/benjamin/dugas/maru/MeetingListTest.java
@@ -1,5 +1,6 @@
 package com.benjamin.dugas.maru;
 
+import androidx.test.espresso.Espresso;
 import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -17,6 +18,9 @@ import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static com.benjamin.dugas.maru.utils.RecyclerViewItemCountAssertion.withItemCount;
@@ -46,20 +50,6 @@ public class MeetingListTest {
     }
 
     @Test
-    public void clickOnActionBouton_shouldShowAddMeeting() {
-        onView(withId(R.id.fab_add)).perform(click());
-        onView(withId(R.id.add)).check(matches(ViewMatchers.isDisplayed()));
-    }
-
-    @Test
-    public void myMeetingList_deleteAction_shouldRemoveItem() {
-        int count = service.getMeeting().size();
-        onView(withId(R.id.meeting_list)).check(withItemCount(count));
-        onView(withId(R.id.meeting_list)).perform(RecyclerViewActions.actionOnItemAtPosition(0, new DeleteViewAction()));
-        onView(withId(R.id.meeting_list)).check(withItemCount(count-1));
-    }
-
-    @Test
     public void myMeetingList_filterAction_shouldShowFilterItem() {
         int count = service.getMeeting().size();
         onView(withId(R.id.meeting_list)).check(withItemCount(count));
@@ -71,5 +61,29 @@ public class MeetingListTest {
         onView(ViewMatchers.withText("Location")).perform(click());
         onView(ViewMatchers.withText("A")).perform(click());
         onView(withId(R.id.meeting_list)).check(withItemCount(count-2));
+    }
+
+    @Test
+    public void myMeetingList_deleteAction_shouldRemoveItem() {
+        int count = service.getMeeting().size();
+        onView(withId(R.id.meeting_list)).check(withItemCount(count));
+        onView(withId(R.id.meeting_list)).perform(RecyclerViewActions.actionOnItemAtPosition(0, new DeleteViewAction()));
+        onView(withId(R.id.meeting_list)).check(withItemCount(count-1));
+    }
+
+    @Test
+    public void clickOnActionBouton_shouldShowAddMeeting() {
+        int count = service.getMeeting().size();
+        onView(withId(R.id.meeting_list)).check(withItemCount(count));
+        onView(withId(R.id.fab_add)).perform(click());
+        onView(withId(R.id.add)).check(matches(ViewMatchers.isDisplayed()));
+        onView(withId(R.id.tiet_topic)).perform(typeText("Test")).perform(closeSoftKeyboard());
+        onView(withId(R.id.rb_a)).perform(click());
+        onView(withId(R.id.tiet_participant)).perform(typeText("Sacha"));
+        onView(withId(R.id.b_add)).perform(click());
+        onView(withId(R.id.tiet_participant)).perform(typeText("Pierre")).perform(closeSoftKeyboard());
+        onView(withId(R.id.add)).perform(scrollTo());
+        onView(withId(R.id.b_create)).perform(scrollTo()).perform(click());
+        onView(withId(R.id.meeting_list)).check(withItemCount(count+1));
     }
 }
